9c7b0f37acc9ea4661bb24ac5f84a92bd55bb064
apaar07 <144550525+apaar07@users.noreply.github.com>
2024-09-12 21:33:28 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -9,34 +9,34 @@ pswd_numbers = int(input("how many numbers would u like in your password ?"))
 
 # easy
 
-# password = ""
-# for i in range(0,pswd_characters):
-#         password += random.choice(letters)
+password = ""
+for i in range(0,pswd_characters):
+        password += random.choice(letters)
 
-# for i in range(0,pswd_symbols):
-#         password += random.choice(symbols)
+for i in range(0,pswd_symbols):
+        password += random.choice(symbols)
 
-# for i in range(0,pswd_numbers):
-#         password += random.choice(numbers)
+for i in range(0,pswd_numbers):
+        password += random.choice(numbers)
 
-# print("your required password is : " , password)
+print("your required password is : " , password)
 
 # hard
 
-# password_list = []
-# for i in range(0,pswd_characters):
-#         password_list.append(random.choice(letters))
+password_list = []
+for i in range(0,pswd_characters):
+        password_list.append(random.choice(letters))
 
-# for i in range(0,pswd_symbols):
-#         password_list.append(random.choice(symbols))
+for i in range(0,pswd_symbols):
+        password_list.append(random.choice(symbols))
 
-# for i in range(0,pswd_numbers):
-#         password_list.append(random.choice(numbers))
+for i in range(0,pswd_numbers):
+        password_list.append(random.choice(numbers))
 
-# # print(password_list)
-# random.shuffle(password_list)
+# print(password_list)
+random.shuffle(password_list)
 
-# password = ""
-# for i in password_list:
-#         password+=i
-# print("your required password is : ",password)
+password = ""
+for i in password_list:
+        password+=i
+print("your required password is : ",password)
